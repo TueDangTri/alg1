ff303ead20444d0765313f9ee02a2070ff06d2bd
TriTueDang <tri.tue.dang@tul.cz>
2023-11-08 22:27:14 +0100

    Playing with a for loop

---
+++ src/start/ForLoop.java
@@ -0,0 +1,16 @@
+package start;
+
+public class ForLoop {
+	
+	public static void main (String[] args) {
+		
+		System.out.println("Program for counting from 1 to 100.");
+		for (int i = 1; i <= 100; i++) {
+			if (i == 53) {
+				System.out.println();
+			}
+			System.out.print(i + " ");
+		}
+	}
+	
+}
