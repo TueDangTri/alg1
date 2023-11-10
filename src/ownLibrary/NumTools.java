9bd2b50ea13091c54b2b66ea356a193fa4efb3fa
TriTueDang <tri.tue.dang@tul.cz>
2023-11-10 21:14:58 +0100

    Added formatting

--- src/ownLibrary/NumTools.java
+++ src/ownLibrary/NumTools.java
@@ -1,18 +1,21 @@
 package ownLibrary;
 
 public final class NumTools {
-	private NumTools(){
-		
+	private NumTools() {
+
 	}
+
 	/**
-	 * Determining the number of divisors of a number (beside 1 and the number itself)
+	 * Determining the number of divisors of a number (beside 1 and the number
+	 * itself)
+	 * 
 	 * @param num
 	 * @return
 	 */
-	
+
 	public static int numDivisors(int num) {
 		int count = 0;
-		int max = num / 2 ;
+		int max = num / 2;
 		for (int i = 2; i <= max; i++) {
 			if (num % i == 0) {
 				count++;
@@ -21,9 +24,9 @@ public final class NumTools {
 
 		return count;
 	}
-	
+
 	public static boolean isPrime(int num) {
-		
+
 		if (num < 2)
 			return false;
 		if (num == 2 || num == 3)
@@ -31,28 +34,30 @@ public final class NumTools {
 		if (num % 2 == 0)
 			return false;
 		int max = (int) Math.sqrt(num);
-		for (int i = 3; i <= max + 1; i+=2 ) {
+		for (int i = 3; i <= max + 1; i += 2) {
 			if (num % i == 0)
 				return false;
 		}
 		return true;
-		
+
 	}
+
 	/**
 	 * Greatest common divisor
+	 * 
 	 * @param a
 	 * @param b
 	 * @return return the biggest divisor of two numbers
 	 */
-	
+
 	public static int gcd(int a, int b) {
 		int max;
 		int min;
-		
+
 		if (a >= b) {
 			max = a;
 			min = b;
-		}else {
+		} else {
 			max = b;
 			min = a;
 		}
@@ -66,36 +71,36 @@ public final class NumTools {
 			}
 		}
 		return min;
-			
-		
+
 	}
-	
+
 	/**
 	 * Least common multiple
+	 * 
 	 * @param a
 	 * @param b
 	 * @return
 	 */
 	public static int lcm(int a, int b) {
-		
-		return (a*b) / gcd (a, b);
-		
+
+		return (a * b) / gcd(a, b);
+
 	}
-	
+
 	/**
 	 * Factorial of a number
+	 * 
 	 * @param num
 	 * @return
 	 */
 	public static long fact(int num) {
 		long result = 1;
-		
-		for(int i = 2; i <= num; i++) {
+
+		for (int i = 2; i <= num; i++) {
 			result *= i;
 		}
-		
+
 		return result;
 	}
-	
-	
+
 }
