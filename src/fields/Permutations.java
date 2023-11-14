ecef9912d7f71ce9c5bb2203650c92f2679a9079
TriTueDang <tri.tue.dang@tul.cz>
2023-11-14 10:45:51 +0100

    pseudo-random number permutations in static array

---
+++ src/fields/Permutations.java
@@ -0,0 +1,27 @@
+package fields;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Permutations {
+
+	private static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Zadej delku pole: ");
+		int len = sc.nextInt();
+		int arr[] = new int[len];
+		
+		Random num = new Random();
+		int numAdd = Math.abs(num.nextInt() % len) + 1;
+		
+		for (int i = 0; i < len; i++) {
+			arr[i] = (numAdd++) % len;
+		}
+		
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+
+}
